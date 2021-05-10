50c43c73302259f9ae840989ed593634ed70f2dc
stack-wuh <wuh131420@foxmail.com>
2021-05-10 14:41:44 +0800

    feat(webpack): 新增web-webpack-plugin和splitChunks配置

--- demo3/webpack.config.js
+++ demo3/webpack.config.js
@@ -1,7 +1,12 @@
+const { WebPlugin } = require('web-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = {
-    entry: './index.jsx',
+    entry: {
+      app: './index.jsx'
+    },
     output: {
-        filename: 'bundle.js'
+        filename: '[name].[contenthash:8].js'
     },
 	devServer: {
 		port: 9000
@@ -14,5 +19,35 @@ module.exports = {
                 use: 'babel-loader'
             }
         ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new WebPlugin({
+        filename: 'web-plugin-template.html',
+        template: './index.html',
+      }),
+      new WebPlugin({
+        filename: 'page1.html',
+        template: './index.html',
+        requires: ['app']
+      })
+    ],
+    optimization: {
+      splitChunks: {
+        chunks: 'async',
+        minChunks: 1,
+        cacheGroups: {
+          vender: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vender',
+            chunks: 'all'
+          },
+          default: {
+            minChunks: 1,
+            name: 'app',
+            chunks: 'all'
+          }
+        }
+      }
     }
 }
